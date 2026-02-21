e6ad1870a502ac73e7b5fd8dbe0f9fe34e9f54f6
Mohd. Saif Siddiqui <siddiqui16saif@gmail.com>
2026-02-21 11:53:40 +0530

    fix: allow cross-origin cookies for railway deploy

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,7 +22,8 @@ const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: true, // MUST be true for SameSite='none'
+            sameSite: 'none', // Allows cross-origin cookies
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
